af4b5ef8e7872270fca58850fc52712a606388ff
Junichi Ito <jit@sonicgarden.jp>
2017-12-07 10:13:41 +0900

    Add fixtures

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'relationships' do
+    assert_equal 2, Project.count
+    tanaka_project = projects(:tanaka_project)
+    assert_equal '田中工業', tanaka_project.customer.name
+    assert_equal ['Alice', 'Bob'], tanaka_project.members.map(&:name).sort
+    suzuki_project = projects(:suzuki_project)
+    assert_equal '鈴木建設', suzuki_project.customer.name
+    assert_equal ['Alice', 'Carol', 'Dave'], suzuki_project.members.map(&:name).sort
+  end
 end
