72ec3d6af175b3aa95f7a542baabd45b70a582c8
Junichi Ito <jit@sonicgarden.jp>
2017-12-08 06:34:13 +0900

    Avoid N+1 problem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController < ApplicationController
   def index
     @project_search_form = ProjectSearchForm.new(project_search_form_params)
-    @projects = @project_search_form.result.order(:id)
+    @projects = @project_search_form.result.includes(:customer, :members).order(:id)
   end
 
   private
