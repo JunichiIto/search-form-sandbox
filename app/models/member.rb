8f76383208e0322f1e5142c9bd1b5770d766aee2
Junichi Ito <jit@sonicgarden.jp>
2017-12-07 10:01:51 +0900

    Add validations and relationships

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ApplicationRecord
+  has_many :memberships
+  has_many :projects, through: :memberships
+
+  validates :name, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :customer
+  has_many :memberships
+  has_many :members, through: :memberships
+
+  validates :name, presence: true
 end
